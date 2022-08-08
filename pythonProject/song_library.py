d327f8d5e6d3c335889fe6fcb19b0de5131c346e
Yotam Shkedi <yotamshkedi@gmail.com>
2022-08-08 16:52:53 +0300

    create song codes list csv file when regenerating song codes

--- pythonProject/song_library.py
+++ pythonProject/song_library.py
@@ -4,21 +4,30 @@ import random
 from time import sleep
 
 import eyed3
-
+import csv
 import code_generator
 
 SONGS_FOLDER_NAME = "songs/"
 
 GENERAL_NAME = "general/"
 MORNING_NAME = "morning/"
-ALL_COLLECTIONS = (GENERAL_NAME, MORNING_NAME)
+PARTY_NAME = "party/"
+TV_NAME = "tv/"
+CLASSIC_NAME = "classic/"
+
+ALL_COLLECTIONS = [GENERAL_NAME, MORNING_NAME, CLASSIC_NAME, TV_NAME, PARTY_NAME]
 
 SCHEDULE = (
-    (datetime.time(0, 0), [GENERAL_NAME]),
-    (datetime.time(6, 0), [MORNING_NAME]),
-    (datetime.time(8, 0), [GENERAL_NAME]),
-    (datetime.time(9), [GENERAL_NAME]),
-    (datetime.time(23,45), [MORNING_NAME]),
+    (datetime.time(0, 0), [PARTY_NAME]),
+    (datetime.time(2, 0), [[GENERAL_NAME, PARTY_NAME, MORNING_NAME, CLASSIC_NAME]]),
+    (datetime.time(6, 0), [CLASSIC_NAME]),
+    (datetime.time(8, 0), [MORNING_NAME]),
+    (datetime.time(10), [GENERAL_NAME, PARTY_NAME, MORNING_NAME, CLASSIC_NAME]),
+    (datetime.time(11), [TV_NAME]),
+    (datetime.time(12), [GENERAL_NAME, PARTY_NAME, MORNING_NAME, CLASSIC_NAME]),
+    (datetime.time(16), [TV_NAME]),
+    (datetime.time(17), ALL_COLLECTIONS),
+    (datetime.time(18), [GENERAL_NAME, PARTY_NAME, MORNING_NAME, CLASSIC_NAME]),
 )
 
 
@@ -31,20 +40,24 @@ def find_schedule_index(current_time: datetime.datetime):
 
 
 def regenerate_codes_all_songs():
-    s = set()
-    for collection in ALL_COLLECTIONS:
-        for filename in os.listdir(SONGS_FOLDER_NAME + collection):
-            songpath = SONGS_FOLDER_NAME + collection + filename
-            print(f"Generating new code for: {songpath}")
-            file = eyed3.load(songpath)
-            new_code = code_generator.generate_new_code()
-            while new_code in s:
+    with open('AmplifierSongCodes.csv', 'w', newline='') as database_file:
+        writer = csv.writer(database_file)
+        writer.writerow(["Song Name", "Code"])
+        s = set()
+        for collection in ALL_COLLECTIONS:
+            for filename in os.listdir(SONGS_FOLDER_NAME + collection):
+                songpath = SONGS_FOLDER_NAME + collection + filename
+                print(f"Generating new code for: {songpath}")
+                audio_file = eyed3.load(songpath)
                 new_code = code_generator.generate_new_code()
-            if not file.tag:
-                file.tag = eyed3.id3.tag.Tag()
-            file.tag.album = new_code
-            file.tag.save()
-            s.add(new_code)
+                while new_code in s:
+                    new_code = code_generator.generate_new_code()
+                if not audio_file.tag:
+                    audio_file.tag = eyed3.id3.tag.Tag()
+                audio_file.tag.album = new_code
+                audio_file.tag.save(version=eyed3.id3.tag.ID3_V2_3)
+                s.add(new_code)
+                writer.writerow([filename, new_code])
 
 
 class SongChooser:
